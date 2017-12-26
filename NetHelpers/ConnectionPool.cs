629cb89520b716879b1cc007c9e1a7cbaa0faf7b
Kirill19837 <34861629+Kirill19837@users.noreply.github.com>
2017-12-26 16:16:02 +0200

    Initial commit

---
+++ NetHelpers/ConnectionPool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace NetHelpers
+{
+    public class ConnectionPool
+    {
+        public string PoolIdentifier { get; internal set; }
+        public int NumberOfConnections { get; internal set; }
+        public static ConnectionPool GetConnectionPool(SqlConnection sqlConnection)
+        {
+            ConnectionPool connectionPool = new ConnectionPool();
+            connectionPool.PoolIdentifier = sqlConnection.ConnectionString;
+
+            Type sqlConnectionType = typeof(SqlConnection);
+            FieldInfo _poolGroupFieldInfo =
+              sqlConnectionType.GetField("_poolGroup", BindingFlags.NonPublic | BindingFlags.Instance);
+            var dbConnectionPoolGroup =
+              _poolGroupFieldInfo.GetValue(sqlConnection);
+
+            if (dbConnectionPoolGroup != null)
+            {
+
+                FieldInfo _poolCollectionFieldInfo =
+                  dbConnectionPoolGroup.GetType().GetField("_poolCollection",
+                     BindingFlags.NonPublic | BindingFlags.Instance);
+
+                var poolCollection = _poolCollectionFieldInfo.GetValue(dbConnectionPoolGroup) as IEnumerable;
+                foreach (var poolEntry in poolCollection)
+                {
+                    var t = poolEntry.GetType();
+                    var foundPool = t.GetProperty("Value").GetValue(poolEntry, null);
+                    FieldInfo _objectListFieldInfo =
+                       foundPool.GetType().GetField("_objectList",
+                          BindingFlags.NonPublic | BindingFlags.Instance);
+                    var listTDbConnectionInternal =
+                       _objectListFieldInfo.GetValue(foundPool);
+                    MethodInfo get_CountMethodInfo =
+                        listTDbConnectionInternal.GetType().GetMethod("get_Count");
+                    var numberOfConnections = get_CountMethodInfo.Invoke(listTDbConnectionInternal, null);
+                    connectionPool.NumberOfConnections = (Int32)numberOfConnections;
+                }
+            }
+            return connectionPool;
+        }
+    }
+}
